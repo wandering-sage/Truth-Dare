72a8897c2310edefc8b1adf0ca561241e09f638a
wandering-sage <shivam4503@gmail.com>
2020-12-24 16:11:33 +0530

    Updated

--- src/js/new.js
+++ src/js/new.js
@@ -1,4 +1,4 @@
-var truthQues = [
+const truthQues = [
 	"What are your top three turn-ons?",
 	"What is your deepest darkest fear?",
 	"What is the biggest lie you have ever told?",
@@ -11,11 +11,10 @@ var truthQues = [
 	"What's the most drunk you've ever been?",
 	"Have you ever stayed friends with someone because it benefitted you beyond just the friendship?",
 ];
-var dareQues = [
+const dareQues = [
 	"Show the last five people you texted and what the messages said",
 	"Say something dirty to the person on your left",
 	"Keep your eyes closed until it's your go again",
-	"Send a sext to the last person in your phonebook",
 	"Pole dance with an imaginary pole",
 	"Talk in an accent for the next 3 rounds.",
 	"Attempt to do a magic trick.",
@@ -23,28 +22,38 @@ var dareQues = [
 	"Write or draw something embarrassing somewhere on your body with a permanent marker.",
 	"Let the person to your left draw on your face with a pen.",
 	"Seduce a member of the same gender in the group.",
+	"Send a sext to the last person in your phonebook",
 	"Choose someone from the group to give you a spanking.",
 ];
 var players = [];
-addPlayers();
 
-var outsideRadius = 250;
+// declaring variables
+var wheelRadius = 250;
 var pi = Math.PI;
-let startAngle = 0;
-let angle = 0;
+var startAngle = 0;
+var angle = 0;
+var spinAngleStart = 0;
+var spinTimeTotal = 0;
+var spinTime = 0;
+var spinTimeout;
 var arc = 0;
 var ctx;
+const canvas = document.querySelector(".wheel");
+
+addPlayers();
+drawWheel();
 
 const addBtn = document.querySelector(".btn-add");
 addBtn.addEventListener("click", addPlayers);
 
-const canvas = document.querySelector(".wheel");
-drawWheel();
-
 const spinBtn = document.querySelector(".btn-spin");
 spinBtn.addEventListener("click", spin);
 
+// **************************Functions***************************************
+// **************************************************************************
+
 function addPlayers() {
+	// openPopup returns a destructor function
 	let closePopup = openPopup();
 	let popup = document.querySelector(".popup");
 	popup.appendChild(createEl("h2", "Add Players (Atleast 2)"));
@@ -57,6 +66,7 @@ function addPlayers() {
 	});
 
 	popup.appendChild(createEl("label", "Name: "));
+
 	let inp = document.createElement("input", HTMLInputElement);
 	inp.type = "text";
 	inp.className = "inp-name";
@@ -84,6 +94,8 @@ function addPlayers() {
 		}
 	});
 
+	// *****************Functions******************
+
 	function closeAndDraw() {
 		closePopup();
 		drawWheel();
@@ -98,6 +110,7 @@ function addPlayers() {
 function tdPopup(person) {
 	spinBtn.disabled = false;
 
+	// openPopup returns a destructor function
 	let closePopup = openPopup();
 	let popup = document.querySelector(".popup");
 
@@ -107,53 +120,49 @@ function tdPopup(person) {
 	let TruthBtn = document.createElement("button", HTMLButtonElement);
 	TruthBtn.innerText = "Truth!";
 	TruthBtn.className = "btn btn-truth";
-	TruthBtn.onclick = showTruth;
+	TruthBtn.onclick = showQues("Truth");
 	popup.appendChild(TruthBtn);
 
 	let DareBtn = document.createElement("button", HTMLButtonElement);
 	DareBtn.innerText = "Dare!";
 	DareBtn.className = "btn btn-dare";
-	DareBtn.onclick = showDare;
+	DareBtn.onclick = showQues("Dare");
 	popup.appendChild(DareBtn);
 
-	function showTruth() {
-		closePopup();
-		let closeIt = openPopup();
-		let popup = document.querySelector(".popup");
+	// *****************Functions*****************
 
-		popup.appendChild(createEl("h2", `Truth`));
-		popup.appendChild(
-			createEl(
-				"p",
-				`${truthQues[Math.floor(Math.random() * truthQues.length)]}`
-			)
-		);
+	function showQues(type) {
+		function ret() {
+			closePopup();
+			// openPopup returns a destructor function
+			let closeIt = openPopup();
+			let popup = document.querySelector(".popup");
 
-		let doneBtn = document.createElement("button", HTMLButtonElement);
-		doneBtn.innerText = "Done";
-		doneBtn.className = "btn btn-done";
-		doneBtn.onclick = closeIt;
-		popup.appendChild(doneBtn);
-	}
+			popup.appendChild(createEl("h2", `${type}`));
 
-	function showDare() {
-		closePopup();
-		let closeIt = openPopup();
-		let popup = document.querySelector(".popup");
+			// checks which ques to put in the current popup
+			popup.appendChild(
+				createEl(
+					"p",
+					`${
+						type == "Dare"
+							? shuffle(dareQues)[
+									Math.floor(Math.random() * dareQues.length)
+							  ]
+							: shuffle(truthQues)[
+									Math.floor(Math.random() * truthQues.length)
+							  ]
+					}`
+				)
+			);
 
-		popup.appendChild(createEl("h2", `Dare`));
-		popup.appendChild(
-			createEl(
-				"p",
-				`${dareQues[Math.floor(Math.random() * dareQues.length)]}`
-			)
-		);
-
-		let doneBtn = document.createElement("button", HTMLButtonElement);
-		doneBtn.innerText = "Done";
-		doneBtn.className = "btn btn-done";
-		doneBtn.onclick = closeIt;
-		popup.appendChild(doneBtn);
+			let doneBtn = document.createElement("button", HTMLButtonElement);
+			doneBtn.innerText = "Done";
+			doneBtn.className = "btn btn-done";
+			doneBtn.onclick = closeIt;
+			popup.appendChild(doneBtn);
+		}
+		return ret;
 	}
 }
 
@@ -204,6 +213,7 @@ function drawWheel() {
 	arc = (2 * pi) / l;
 	let textRadius = 170;
 
+	// a loop to draw equal sectors for each player on the wheel
 	for (let i = 0; i < l; i++) {
 		var text = players[i];
 		angle = startAngle + i * arc;
@@ -214,10 +224,11 @@ function drawWheel() {
 		ctx.fillStyle = wheelColors[i % 3];
 
 		ctx.beginPath();
-		ctx.arc(260, 260, outsideRadius, angle, angle + arc, false);
+		ctx.arc(260, 260, wheelRadius, angle, angle + arc, false);
 		ctx.lineTo(260, 260);
 		ctx.fill();
 
+		// to write the name of the player on that sector
 		ctx.save();
 		ctx.fillStyle = wheelColors[(i + 1) % 3];
 		ctx.translate(
@@ -230,37 +241,43 @@ function drawWheel() {
 		ctx.restore();
 	}
 
-	// to draw circle
+	// to draw circle outlining the wheel
 	ctx.beginPath();
-	ctx.arc(260, 260, outsideRadius, 0, 2 * pi);
+	ctx.arc(260, 260, wheelRadius, 0, 2 * pi);
 	ctx.lineWidth = 3;
 	ctx.stroke();
 
-	// to draw triangle
+	// to draw triangle on right side of wheel
 	ctx.fillStyle = "rgba(255, 251, 0, 0.4)";
 	ctx.beginPath();
-	ctx.moveTo(260 + outsideRadius - 40, 260);
-	ctx.lineTo(260 + outsideRadius + 40, 260 - 20);
-	ctx.lineTo(260 + outsideRadius + 40, 260 + 20);
+	ctx.moveTo(260 + wheelRadius - 40, 260);
+	ctx.lineTo(260 + wheelRadius + 40, 260 - 20);
+	ctx.lineTo(260 + wheelRadius + 40, 260 + 20);
 	ctx.closePath();
 	ctx.stroke();
 	ctx.fill();
 }
 
 function spin() {
+	// so that user cant press spin button twice or more
 	spinBtn.disabled = true;
+
 	spinAngleStart = Math.random() * 10 + 10;
 	spinTime = 0;
+
+	// total spin time comes b/w 3sec to 5sec
 	spinTimeTotal = Math.random() * 2000 + 3 * 1000;
 	rotateWheel();
 }
 
+// this function will run in loop till spinTime exceeds total spin time
 function rotateWheel() {
 	spinTime += 10;
 	if (spinTime >= spinTimeTotal) {
-		stopRotateWheel();
+		stopWheelAndGetPlayer();
 		return;
 	}
+	// to slow down the wheel near end by reducing the angle of rotation per loop
 	var spinAngle =
 		spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
 	startAngle += (spinAngle * pi) / 180;
@@ -268,7 +285,7 @@ function rotateWheel() {
 	spinTimeout = setTimeout("rotateWheel()", 10);
 }
 
-function stopRotateWheel() {
+function stopWheelAndGetPlayer() {
 	clearTimeout(spinTimeout);
 	var degrees = (startAngle * 180) / pi;
 	var arcd = (arc * 180) / pi;
@@ -280,4 +297,14 @@ function easeOut(t, b, c, d) {
 	var ts = (t /= d) * t;
 	var tc = ts * t;
 	return b + c * (tc + -3 * ts + 3 * t);
+}
+
+function shuffle(array) {
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	return array;
 }
